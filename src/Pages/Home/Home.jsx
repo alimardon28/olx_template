81dbe3567564b95d46734d94e2fb2590377e9a8b
alimardon28 <doniyorovalimardon369@gmail.com>
2022-06-10 17:27:23 +0500

    olx clone react.js initional commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Home/Home.scss";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Home pages</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Pages/Products/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Products/Product.scss';
+
+const Product = () => {
+    return (
+        <div className='product'>
+              <h1>product pages</h1>
+        </div>
+    );
+}
+
+export default Product;
