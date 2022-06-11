958b2a3141438058a780aa850fbaa4ab41c68e0b
alimardon28 <doniyorovalimardon369@gmail.com>
2022-06-11 10:23:22 +0500

    header components finished

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,69 @@
-import React from 'react';
-import '../Header/Header.scss';
+import React from "react";
+import "../Header/Header.scss";
+import { Link } from "react-router-dom";
+import logo from "../../assets/svg/olxLogo.png";
+import chat from "../../assets/svg/chat.svg";
+import heart from "../../assets/svg/heart.svg";
+import contact from "../../assets/svg/person.svg";
+import hamburger from "../../assets/images/menu_icon.png";
 
 const Header = () => {
-    return (
-        <div className='header'>
-       <h1>Header</h1>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <div className="container">
+        <nav className="header__navbar">
+          <a className="header__navbar_link" to="/">
+            <img src={logo} alt="olx logo images" />
+          </a>
+          <ul className="header__list">
+            <li className="header__hamburger__menyu">
+              <img src={hamburger} alt="hamburger images" />
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                <span>Uzb</span> |<span>Rus</span>
+              </a>
+            </li>
+            <li className="header__item header__d-block">
+              <a href="#" className="header__link">
+                <i class="bi bi-chat"></i>
+                <p>Xabarlar</p>
+              </a>
+            </li>
+            <li className="header__item header__d-block">
+              <a href="#" className="header__link">
+                <div className="header__heartb0x">
+                  <i class="bi bi-heart"></i>
+                  <span>0</span>
+                </div>
+              </a>
+            </li>
+            <li className="header__item">
+              <a href="#" className="header__link">
+                <i class="bi bi-person"></i>
+                <select
+                  name="select"
+                  id="header__select"
+                  className="header__select"
+                >
+                  <option value="">Mening profilim</option>
+                  <option value="">e'lonlar</option>
+                  <option value="">to'lov va olx xisobi</option>
+                  <option value="">sozlamalar</option>
+                  <option value="">nomzod profili</option>
+                </select>
+              </a>
+            </li>
+            <li className="header__item">
+              <button href="#" className="header__button">
+                E'lon berish
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
